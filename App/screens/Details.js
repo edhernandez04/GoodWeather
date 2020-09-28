ed320d4ae48d8379dba64168711fabc7137941e8
edhernandez04 <mchh.004@gmail.com>
2020-09-28 15:48:27 -0400

    get weather/get forecast "

--- App/screens/Details.js
+++ App/screens/Details.js
@@ -1,23 +1,41 @@
 import React from 'react';
+import Geolocation from '@react-native-community/geolocation';
 
 const apiKey = '982b33b2128414182a9809fc4e83fc98';
 
 export default class Details extends React.Component {
-  componentDidMount() {
-    let zipCode = 10457;
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&zip=${zipCode}&units=metric`,
-    )
-      .then((resp) => resp.json())
-      .then((resp) => {
-        console.log(resp);
-      })
-      .catch((err) => {
-        console.log('error', err);
-      });
+  state = {
+    zipcode: null,
+    coords: null,
+  };
 
-    fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&zip=${zipCode}&units=metric`,
+  componentDidMount() {
+    Geolocation.getCurrentPosition(
+        (position) => {
+          this.getCurrentWeather({
+            coords: {
+              latitude: parseFloat(JSON.stringify(position.coords.latitude)),
+              longitude: parseFloat(JSON.stringify(position.coords.longitude)),
+            },
+          });
+          this.getForecast({
+            coords: {
+              latitude: parseFloat(JSON.stringify(position.coords.latitude)),
+              longitude: parseFloat(JSON.stringify(position.coords.longitude)),
+            },
+          });
+        },
+        (error) => alert(error.message),
+        {enableHighAccuracy: false, timeout: 20000, maximumAge: 10000},
+    )
+  }
+
+  getCurrentWeather = ({zipcode, coords}) => {
+    let suffix = '';
+    if (zipcode) suffix = `zip=${zipcode}`;
+    else if (coords) suffix = `lat=${coords.latitude}&lon=${coords.longitude}`;
+    return fetch(
+      `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&units=imperial&${suffix}`,
     )
       .then((resp) => resp.json())
       .then((resp) => {
@@ -26,7 +44,23 @@ export default class Details extends React.Component {
       .catch((err) => {
         console.log('error', err);
       });
-  }
+  };
+
+  getForecast = ({zipcode, coords}) => {
+    let suffix = '';
+    if (zipcode) suffix = `zip=${zipcode}`;
+    else if (coords) suffix = `lat=${coords.latitude}&lon=${coords.longitude}`;
+    return fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&units=imperial&${suffix}`,
+    )
+      .then((resp) => resp.json())
+      .then((resp) => {
+        console.log(resp);
+      })
+      .catch((err) => {
+        console.log('error', err);
+      });
+  };
 
   render() {
     return null;
